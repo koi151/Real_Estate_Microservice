597ee4312056ce5a56a3c61f1e980db6c9045e4e
Khôi Nguyễn <originalkhoi151@gmail.com>
2024-08-24 11:40:53 +0700

    Update PostServicePackage entity with columns for storing post service price, price discount, and price discount at creation phase

--- services/listing-services/src/main/java/com/koi151/listing_services/entity/PostServicePackage.java
+++ services/listing-services/src/main/java/com/koi151/listing_services/entity/PostServicePackage.java
@@ -2,9 +2,13 @@ package com.koi151.listing_services.entity;
 
 import com.koi151.listing_services.entity.keys.PostServicePackageKey;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
+
+import java.math.BigDecimal;
 
 @Entity(name = "post_service_package")
 @Getter
@@ -29,4 +33,30 @@ public class PostServicePackage {
     @NotNull(message = "Units remaining quantity is mandatory")
     @Positive(message = "Units remaining quantity must be greater than zero")
     private int unitsRemaining;
+
+    @Column(name = "price_at_creation", columnDefinition = "DEFAULT 0.00")
+    @PositiveOrZero(message = "Post service price must be non-negative value")
+    @DecimalMax(value = "99999999.99", message = "Post service price cannot exceed {value}")
+    private BigDecimal priceAtCreation;
+
+    @Column(name = "discount_percentage_at_creation", columnDefinition = "DEFAULT 0.00")
+    @PositiveOrZero(message = "Discount percentage must be non-negative value")
+    @DecimalMax(value = "99.99", message = "Post service price cannot exceed {value}")
+    private BigDecimal discountPercentageAtCreation;
+
+    @Column(name = "price_discount_at_creation", columnDefinition = "DEFAULT 0.00")
+    @PositiveOrZero(message = "Price discount must be non-negative value")
+    @DecimalMax(value = "99999999.99", message = "Price discount cannot exceed {value}")
+    private BigDecimal priceDiscountAtCreation;
 }
+
+
+
+
+
+
+
+
+
+
+
