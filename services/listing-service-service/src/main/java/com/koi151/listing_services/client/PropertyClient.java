2a9166b56e455ec8a1a341e6e4b730d27e6504c2
Khôi Nguyễn <originalkhoi151@gmail.com>
2024-10-15 20:51:04 +0700

    updated FeignClient url value

--- services/listing-service-service/src/main/java/com/koi151/listing_services/client/PropertyClient.java
+++ services/listing-service-service/src/main/java/com/koi151/listing_services/client/PropertyClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "property-service", url = "${application.config.properties-url}")
+@FeignClient(name = "property-service", url = "http://property-service:8050")
 public interface PropertyClient {
     @GetMapping("/{id}/active")
     ResponseEntity<ResponseData> propertyExistsCheck(@PathVariable(name = "id") Long id);
